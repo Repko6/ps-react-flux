d31092e5b0e5e5a4527e68748b3d8dd5b77e520b
repko6 <jure.perak@hotmail.com>
2020-04-19 18:13:17 +0200

    React router implemented. Link, NavLink, Redirect, Route

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React from "react"; //import npm package 'react' to variable React
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="jumbotron">
       <h1>Pluralsight administration</h1>
       <p>React, Flux and React Router for ultra-responsive web apps.</p>
-      <a href="/about">About</a>
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 }
